706c6cdd710a664437c8d3ec5b7d9b9a1e2c7fc3
guhZamp <gustavozampellifranco@gmail.com>
2025-03-13 15:46:38 -0300

    gitignore node.modules and change port

--- node.js
+++ node.js
@@ -38,6 +38,7 @@ server.patch("/", async (req, res) => {
   res.status(201).send()
 })
 
-server.listen( {
-    port:3333,
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`)
 })
