5578b2453eeb08d0ee1f4f1dfeeef6bb80810992
guhZamp <gustavozampellifranco@gmail.com>
2025-03-14 16:25:58 -0300

    fixing all paths

--- public/script/cardapioScript/services/api.js
+++ public/script/cardapioScript/services/api.js
@@ -1,5 +1,5 @@
 async function getDescription() {
-    const url = "/sinai-cafe/json/cafes.json"
+    const url = "../../json/cafes.json"
     const result = await fetch(url)
     return result.json();
 }
