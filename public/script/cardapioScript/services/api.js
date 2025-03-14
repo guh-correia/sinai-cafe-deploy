d0c86828a6116ef3ab6f06194c331997d885a7ee
guhZamp <gustavozampellifranco@gmail.com>
2025-03-14 17:05:52 -0300

    fixing all paths

--- public/script/cardapioScript/services/api.js
+++ public/script/cardapioScript/services/api.js
@@ -1,7 +1,10 @@
 async function getDescription() {
     const url = "../json/cafes.json"
-    const result = await fetch(url)
-    return result.json();
+    const result = await fetch('/json/cafes.json')
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(error => console.error('Erro ao carregar o JSON:', error));
+    return result;
 }
 
 export { getDescription }
