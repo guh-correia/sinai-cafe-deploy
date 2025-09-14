35db0c2205c27113f42bf8d3beaea37e2d2090ab
guhZamp <gustavozampellifranco@gmail.com>
2025-09-14 10:19:46 -0300

    options

--- php/index.php
+++ php/index.php
@@ -1,15 +1,15 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "GET") {
-    session_start();
-    $_SESSION["leite"] = $_GET["tipo-leite"];
-    $_SESSION["adicional"] = $_GET["tipo-adicional"];
-    $_SESSION["cafe"] =  $_GET["cafe"];
+// if ($_SERVER["REQUEST_METHOD"] == "GET") {
+//     session_start();
+//     $_SESSION["leite"] = $_GET["tipo-leite"];
+//     $_SESSION["adicional"] = $_GET["tipo-adicional"];
+//     $_SESSION["cafe"] =  $_GET["cafe"];
 
-    header("Location: /sinai-cafe/php/confirmacaoPedido.php");
-    exit;
-} else {
-    header("Location: https://sinai-cafe.vercel.app/");
-    exit;
-};
+//     header("Location: /sinai-cafe/php/confirmacaoPedido.php");
+//     exit;
+// } else {
+//     header("Location: https://sinai-cafe.vercel.app/");
+//     exit;
+// };
 
 ?>
