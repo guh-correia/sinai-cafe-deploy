a15ffe1ccb84406fd9c687629b75e06c53ee0021
guhZamp <gustavozampellifranco@gmail.com>
2025-10-18 08:53:56 -0300

    importing pedidoFeito()

--- public/script/pedidoFinalizadoScript/getOrder.js
+++ public/script/pedidoFinalizadoScript/getOrder.js
@@ -1,5 +1,4 @@
 import { getName } from "./services/getData.js";
-import { pedidoFeito } from "/displayPedidoFeito.js";
 
 document.getElementById("nome-cliente").addEventListener("input", function(event) {
     this.value = this.value.replace(/[^A-Za-zÀ-ÿ\s]/g, "");
@@ -9,7 +8,6 @@ document.getElementById("nome-cliente").addEventListener("keypress", function(ev
     if (event.key === "Enter") {
         event.preventDefault(); // Impede que a quebra de linha aconteça
         getName()
-        pedidoFeito()
     }
 })
 
