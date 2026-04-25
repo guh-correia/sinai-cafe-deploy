9c4a04dc0e1129e29a48799149b32e55398430fa
guhZamp <gustavozampellifranco@gmail.com>
2026-04-25 08:38:29 -0300

    adding chocolate hot and cold

--- public/script/cardapioScript/services/displayVeganCard.js
+++ public/script/cardapioScript/services/displayVeganCard.js
@@ -2,6 +2,7 @@ import { cardAdicionais } from "../dom/cardapio";
 import { centralizeCardAdicionais } from "./centralizeWindow";
 
 function displayVeganCard () {
+    console.log("oi")
     cardAdicionais.querySelector("h1")[0].innerText = "Sua bebida é vegana ou tradicional?"
     cardAdicionais.querySelector("span")[0].innerText = "Escholha uma opção:"
     cardAdicionais.querySelector("span")[1].innerText = "Normal"
