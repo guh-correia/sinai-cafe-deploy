d5bded178efb84a542591bbc39e3bb1689cf9dfc
guhZamp <gustavozampellifranco@gmail.com>
2026-06-05 22:58:00 +0500

    adding frappes

--- public/script/cardapioScript/services/liberacaoFrapes.js
+++ public/script/cardapioScript/services/liberacaoFrapes.js
@@ -1,9 +1,12 @@
 const horarioLiberacao = new Date("2026-06-15T20:00:00");
+const agora = new Date()
 
 function liberacaoFrapre(cafe) {
     if (cafe.classList.contains("frappe")) {
         console.log(horarioLiberacao);
-        return new Date() >= horarioLiberacao;
+        console.log(agora)
+        console.log(agora >= horarioLiberacao)
+        return agora >= horarioLiberacao;   
     }
     return true
 }
